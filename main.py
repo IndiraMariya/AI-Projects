9acaa25366008ad0ff86f86e6ed67b67022b6369
Indira M <indira.mariya@gmail.com>
2023-01-06 19:36:48 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+
+(x_train, y_train) = keras.datasets.mnist.load_data('/Users/indiram/Neural Network/mnist_train.csv')
+(x_test, y_test) = keras.datasets.mnist.load_data('/Users/indiram/Neural Network/mnist_test.csv')
+
+
+/Users/indiram/Neural Network/main.py
